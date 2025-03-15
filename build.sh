d47d383ea65b0b7a3dc1f6f99881783d48ec50d0
sev <git@sev.monster>
2025-03-15 07:40:18 -0500

    update to 2.41, update build, rm unnecessary files
    the build.sh script will now produce a versioned tarball. this is in
    preparation for CI to build and maintain the package version.

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
-docker run --rm --env STDOUT=1 sgerrand/glibc-builder 2.39 /usr/glibc-compat >! glibc-bin.tar.gz
+#!/bin/sh
+if [ -z "$1" -o -z "$2" ]; then
+    echo 'error: must specify package version
+
+usage: build.sh <glibc-version> <pkgrel>
+example: build.sh 2.41 0' >&2
+    exit 1
+fi
+
+docker run --rm --env STDOUT=1 \
+  sgerrand/glibc-builder $1 /usr/glibc-compat \
+  > "glibc-bin-$1-r$2.tar.gz"
+
+# vim: sts=4 sw=4 et
